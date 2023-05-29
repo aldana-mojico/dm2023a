46a34f9d44a47795f99ef221ae2b2f53d633d151
aldana-mojico <aldanamojico@gmail.com>
2023-05-29 19:45:00 -0300

    TS y HT

--- src/workflow-colab/z651_HT_lightgbm.r
+++ src/workflow-colab/z651_HT_lightgbm.r
@@ -37,7 +37,7 @@ PARAM$exp_input  <- "TS6410"
 
 PARAM$lgb_crossvalidation_folds  <- 5  #En caso que se haga cross validation, se usa esta cantidad de folds
 
-PARAM$lgb_semilla  <- 102191   #cambiar por su propia semilla
+PARAM$lgb_semilla  <- 211403   #cambiar por su propia semilla
 
 
 #Hiperparametros FIJOS de  lightgbm
@@ -79,7 +79,7 @@ PARAM$bo_lgb <- makeParamSet(
          makeNumericParam("learning_rate",    lower=    0.02, upper=     0.3),
          makeNumericParam("feature_fraction", lower=    0.01, upper=     1.0),
          makeIntegerParam("num_leaves",       lower=    8L,   upper=  1024L),
-         makeIntegerParam("min_data_in_leaf", lower=  100L,   upper= 50000L)
+         makeIntegerParam("min_data_in_leaf", lower=  100L,   upper= 20000L)
         )
 
 
@@ -103,7 +103,7 @@ GrabarOutput  <- function()
 #graba a un archivo los componentes de lista
 #para el primer registro, escribe antes los titulos
 
-exp_log  <- function( reg, arch=NA, folder="./exp/", ext=".txt", verbose=TRUE )
+exp_log  <- function( reg, arch=NA, folder="./exp202107/", ext=".txt", verbose=TRUE )
 {
   archivo  <- arch
   if( is.na(arch) )  archivo  <- paste0(  folder, substitute( reg), ext )
@@ -411,7 +411,7 @@ setwd( PARAM$home )
 
 #cargo el dataset donde voy a entrenar
 #esta en la carpeta del exp_input y siempre se llama  dataset_training.csv.gz
-dataset_input  <- paste0( "./exp/", PARAM$exp_input, "/dataset_training.csv.gz" )
+dataset_input  <- paste0( "./exp202107/", PARAM$exp_input, "/dataset_training.csv.gz" )
 dataset  <- fread( dataset_input )
 
 dataset[ , azar :=  NULL ]
@@ -423,8 +423,8 @@ if( ! ("fold_test"     %in% colnames(dataset) ) ) stop("Error, el dataset no tie
 if( dataset[ fold_train==1, .N ] == 0 ) stop("Error, en el dataset no hay registros con fold_train==1 \n")
 
 #creo la carpeta donde va el experimento
-dir.create( paste0( "./exp/", PARAM$experimento, "/"), showWarnings = FALSE )
-setwd(paste0( "./exp/", PARAM$experimento, "/"))   #Establezco el Working Directory DEL EXPERIMENTO
+dir.create( paste0( "./exp202107/", PARAM$experimento, "/"), showWarnings = FALSE )
+setwd(paste0( "./exp202107/", PARAM$experimento, "/"))   #Establezco el Working Directory DEL EXPERIMENTO
 
 GrabarOutput()
 write_yaml( PARAM, file= "parametros.yml" )   #escribo parametros utilizados
